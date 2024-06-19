f76d8bfe3855d1a3806a8bb4ec88a69295f4c8bb
BohdanObruch <bodan1992@gmail.com>
2024-06-19 11:44:24 +0300

    added 4 tests by sorting

--- test/pages/Base.page.js
+++ test/pages/Base.page.js
@@ -5,6 +5,7 @@ class BasePage {
     async getUrl() { return browser.getUrl(); }
 
     async navigate() {
+        await browser.setWindowSize(1920, 1080);
         await browser.url(this.url);
     }
 }
