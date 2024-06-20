8274725f2eaf4435758d6915000f4f3716425a8a
BohdanObruch <bodan1992@gmail.com>
2024-06-20 17:58:37 +0300

    removed comments and fixed the getSortItem method

--- test/pages/Inventory.page.js
+++ test/pages/Inventory.page.js
@@ -3,31 +3,27 @@ const { BaseSwagLabPage } = require('./BaseSwagLab.page');
 class InventoryPage extends BaseSwagLabPage {
     url = '/inventory.html';
 
-    // eslint-disable-next-line class-methods-use-this
     get headerTitle() { return $('.title'); }
 
-    // eslint-disable-next-line class-methods-use-this
     get inventoryItems() { return $$('.inventory_item'); }
 
-    // eslint-disable-next-line class-methods-use-this
     get addItemToCartBtns() { return $$('[id^="add-to-cart"]'); }
 
-    // eslint-disable-next-line class-methods-use-this
     get inventoryItemsNames() { return $$('.inventory_item_name'); }
 
-    // eslint-disable-next-line class-methods-use-this
     get inventoryItemsPrices() { return $$('.inventory_item_price'); }
 
-    // eslint-disable-next-line class-methods-use-this
     get sort() { return $('.product_sort_container'); }
 
-    // eslint-disable-next-line class-methods-use-this
     get activeOption() { return $('.active_option'); }
 
-    // eslint-disable-next-line class-methods-use-this
+    // getSortItem(value) {
+    //     return $(`.product_sort_container option[value="${value}"]`);
+    // }
     getSortItem(value) {
-        return $(`.product_sort_container option[value="${value}"]`);
+        return $('.product_sort_container').$(`option[value="${value}"]`);
     }
+
 
     async getActiveOptionText() {
         return this.activeOption.getText();
